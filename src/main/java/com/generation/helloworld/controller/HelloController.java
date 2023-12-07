68057673402316765830640e9609df64f8883721
Victoria_Moraes <victoria.candido.mm@gmail.com>
2023-12-07 13:09:15 -0300

    add hello, bsm e aprendizagem

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -13,17 +13,36 @@ public class HelloController {
 		return "<b>Hello Turma 68!!!</b> Segunda frase";
 
 	}
-	
+
 	@GetMapping("/top")
 	public String top() {
 		return "<b>Hello Turma 68 TOP!!!</b>";
 
 	}
-	
 
 	@GetMapping("/bomdia")
 	public String bomDia() {
 		return "<b>Bom dia Turma 68!!!</b>";
 
 	}
+
+	@GetMapping("/helloworld")
+	public String helloWorld() {
+		return "<b>Hello World!</b>";
+
+	}
+
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<b>Lista de BSMs:</b><br>" + "Mentalidade de Crescimento<br>" + "Orientação ao Futuro<br>"
+				+ "Persistência<br>" + "Comunicação<br>" + "Atenção para Detalhes<br>" + "Trabalho em Equipe<br>"
+				+ "Proatividade<br>" + "Responsabilidade Pessoal";
+	}
+
+	@GetMapping("/aprendizagem")
+	public String aprendizagem() {
+		return "<b>Objetivos de aprendizagem da semana:</b><br>" + "Revisar conteúdos de MySQL e Spring<br>"
+				+ "Fazer lista de exercícios MySQL<br>" + "Procurar mini cursos de design";
+	}
+
 }
